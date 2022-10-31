71986c590e75c007be32e94f43e4fa3941b5661b
karlosgliberal <karlosgliberal@gmail.com>
2022-10-31 16:49:43 +0100

    Añadido el patch de cables

---
+++ src/routes/cables/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+  import { onMount } from "svelte";
+
+  let canvas;
+  const initializeRemarkable = () => {
+    console.log("movida d");
+    CABLES.patch = new CABLES.Patch({
+      patch: CABLES.exportedPatch,
+      prefixAssetPath: "../../patch/",
+      glCanvasId: "glcanvas",
+      glCanvasResizeToWindow: false,
+      onError: showError,
+      onPatchLoaded: patchInitialized,
+      onFinishedLoading: patchFinishedLoading,
+      outSidefunction: myFunction,
+
+      canvas: { alpha: true, premultipliedAlpha: true },
+    });
+  };
+  onMount(() => {
+    initializeRemarkable();
+  });
+
+  function showError(errId, errMsg) {
+    alert("An error occured: " + errId + ", " + errMsg);
+  }
+
+  function patchInitialized() {
+    // You can now access the patch object (CABLES.patch), register variable watchers and so on
+  }
+
+  function patchFinishedLoading() {
+    // The patch is ready now, all assets have been loaded
+  }
+  function handleClick() {
+    CABLES.patch.config.patchFunctiontrigger();
+  }
+  function myFunction() {
+    alert("function called!");
+  }
+</script>
+
+<svelte:head>
+  <script src="patch.js" on:load={initializeRemarkable}></script>
+</svelte:head>
+
+<div>
+  <canvas
+    id="glcanvas"
+    width="800"
+    height="400"
+    style="width: 800px; height: 400px;"
+  />
+  <button class="rounded-full ..." on:click={handleClick}>cables</button>
+</div>
