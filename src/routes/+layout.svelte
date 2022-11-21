48104e4bd7dcc2df540f7fca21764baf86640420
karlosgliberal <karlosgliberal@gmail.com>
2022-11-21 12:45:10 +0100

    Primeras prubas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,38 +24,7 @@
     gtag("config", "G-LSTLRM2XQG");
   </script>
 </svelte:head>
-<div class="fixed z-50 top-0 left-0 w-full">
-  <header class="bg-black px-10 items-center text-white">
-    <nav class="flex relative">
-      <div class="w-1/4 h-12 text-lg pt-5 pl-12">
-        <a href="/" class="flex">
-          <span
-            class="pl-2 self-center text-lg font-semibold text-white whitespace-nowrap"
-            >Historia</span
-          ></a
-        >
-      </div>
-      <div class="container flex flex-wrap justify-end mx-auto pt-1 md:pt-2">
-        <div class="w-full md:block md:w-auto">
-          <ul
-            class="flex flex-col mt-2 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium pt-4 bg-black"
-          >
-            <li
-              class="block py-2 pr-4 pl-3 text-white md:hover:text-gray-500 md:p-0"
-            >
-              <a
-                href="/topmenu"
-                rel="external"
-                class="block py-2 pr-4 pl-5 text-white md:hover:text-gray-500 md:p-0 text-lg"
-                >About</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
-</div>
+
 <main>
   <slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-  import Cables from "../lib/Cables.svelte";
-  import Log from "../lib/Log.svelte";
-  import Aitor from "../lib/Aitor.svelte";
+  // import Cables from "../../lib/Cables.svelte";
+  // import Log from "../../lib/Log.svelte";
+  // import Aitor from "../../lib/Aitor.svelte";
 
-  let index, offset, progress;
-  function handleMessage(event) {
-    index = !index;
-  }
+  // let index, offset, progress;
+  // function handleMessage(event) {
+  //   index = !index;
+  // }
 </script>
 
 <svelte:head>
@@ -14,9 +14,103 @@
   <meta name="description" content="Riscanvi " />
 </svelte:head>
 
+<!--atributos body y contenedor del grid -->
+
+<!--portada-->
+<!--atributos body y contenedor del grid -->
+
+<!--portada-->
+<section id="cover">
+  <!--grid-->
+  <div
+    class="min-h-screen grid grid-rows-3 grid-cols-4 lg:grid-cols-9 gap-3 px-3 sm:px-4"
+  >
+    <!--card container-->
+    <div
+      class="col-span-4 md:col-span-9 lg:col-span-5 row-start-1 sm:row-start-2 lg:col-start-3 self-start h-fit rounded  bg-stone-200 text-zinc-800 text-center shadow-xl mt-24 sm:-mt-24 pb-12 mb-8 "
+    >
+      <!--título-->
+      <h1 class="text-base leading-6 tracking-widest md:px-4 pt-24">
+        El acierto en las decisiones de los
+      </h1>
+      <!--parte título destacado-->
+      <h1
+        class="text-xl sm:text-3xl font-mono font-semibold tracking-wider mt-3 "
+      >
+        sistemas automatizados
+      </h1>
+      <!--botón con borde tipo trazo tiene css personalizado-->
+      <button
+        type="button"
+        class="bordeB lined thick  w-11/12 md:w-4/12 text-base font-mono font-bold tracking-widest text-zinc-800 bg-stone-200 hover:bg-stone-100 shadow-xl mt-12  py-4 "
+        >Comenzar</button
+      >
+    </div>
+    <!--footer-->
+    <div
+      class="col-span-4 md:col-span-9 row-start-2 sm:row-start-3 lg:col-start-2 self-center sm:self-end rounded text-zinc-600 px-8 sm:py-8 sm:mb-4 "
+    >
+      <div
+        class="flex flex-wrap sm:flex-nowrap gap-4 sm:gap-24 lg:gap-48 text-sm font-mono text-center sm:text-left"
+      >
+        <div class="flex flex-col w-full hover:text-zinc-400 ">
+          <!--numero decorativo-->
+          <div class="text-xs mb-3">01</div>
+          <!--enlace-->
+          <a href="movida" class=" hover:text-zinc-200 hover:font-semibold">
+            Sobre Bikolabs
+          </a>
+        </div>
+        <div class="flex flex-col w-full  hover:text-zinc-400 ">
+          <div class="text-xs mb-3">02</div>
+          <a href="movida" class=" hover:text-zinc-200 hover:font-semibold">
+            Contacto
+          </a>
+        </div>
+        <div class="flex flex-col w-full  hover:text-zinc-400 ">
+          <div class="text-xs mb-3">03</div>
+          <a href="movida" class=" hover:text-zinc-200 hover:font-semibold">
+            Créditos
+          </a>
+        </div>
+        <div class="flex flex-col w-full  hover:text-zinc-400 ">
+          <div class="text-xs text-transparent mb-3">_</div>
+          <a href="movida" class="hover:text-zinc-200 hover:font-semibold"
+            >© 2022
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- 
 <section>
   <Aitor ancho="900" visible="movid" on:message={handleMessage} />
   <div class="w-full h-screen absolute z-10">
     <Cables step={index} patch="presos" />
   </div>
-</section>
+</section> -->
+<style>
+  .bordeA {
+    border-top-left-radius: 255px 15px;
+    border-top-right-radius: 15px 225px;
+    border-bottom-right-radius: 225px 15px;
+    border-bottom-left-radius: 15px 255px;
+  }
+
+  .bordeA.lined.thick {
+    border: solid 3px #e7e5e4;
+  }
+
+  .bordeB {
+    border-top-left-radius: 255px 15px;
+    border-top-right-radius: 15px 225px;
+    border-bottom-right-radius: 225px 15px;
+    border-bottom-left-radius: 15px 255px;
+  }
+
+  .bordeB.lined.thick {
+    border: solid 3px #27272a;
+  }
+</style>
