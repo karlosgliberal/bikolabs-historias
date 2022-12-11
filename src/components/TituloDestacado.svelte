16ef2a68fb2bd7df28be422e8176c4d0ac3d4401
karlosgliberal <karlosgliberal@gmail.com>
2022-12-11 23:00:36 +0100

    Ajustes pequeños

--- src/components/TituloDestacado.svelte
+++ src/components/TituloDestacado.svelte
@@ -2,6 +2,8 @@
   export let tituloDestacado = "";
 </script>
 
-<h1 class="text-xl sm:text-3xl font-mono font-semibold tracking-wider mt-3 ">
+<h1
+  class="text-xl sm:text-3xl font-mono font-semibold tracking-wider mt-3  pt-6 "
+>
   {tituloDestacado}
 </h1>
