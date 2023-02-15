29470df3c40ed90513dae846761407e2a677f123
karlosgliberal <karlosgliberal@gmail.com>
2023-02-15 21:09:03 +0100

    Igual algo de formato

--- src/components/PastillaCompleta.svelte
+++ src/components/PastillaCompleta.svelte
@@ -1,62 +1,59 @@
 <script>
-  import { fade } from "svelte/transition";
-  import { vis } from "$lib/store.js";
-  import Texto from "./Texto.svelte";
-  import Titulo from "./Titulo.svelte";
+  import { fade } from 'svelte/transition'
+  import { vis } from '$lib/store.js'
+  import Texto from './Texto.svelte'
+  import Titulo from './Titulo.svelte'
 
-  export let visible;
-  export let titulo;
-  export let texto;
-  export let position;
-  export let id;
+  export let visible
+  export let titulo
+  export let texto
+  export let position
+  export let id
 
-  let timer;
+  let timer
 
   function finDesaparecer() {
     timer = setTimeout(() => {
-      vis.verdadero();
-    }, 1000);
+      vis.verdadero()
+    }, 1000)
   }
   // let isVisible = {id} == '1' && {position} == 'center';
-
 </script>
-{#if visible }
-  <div class="w-full h-full z-20 wrapper_{position} screen_{id}">
 
+{#if visible}
+  <div class="w-full h-full z-20 wrapper_{position} screen_{id}">
     <div
       transition:fade={{ duration: 1500 }}
       on:outroend={finDesaparecer}
       class="z-20 position_{position} fine_{id} h-fit rounded text-white bg-black bg-opacity-75 shadow-xl"
     >
       {#if position == 'center'}
-        {#if titulo != ""}
-         {#if id == '1'}
-              <h2 class="text-3xl tablet:text-6xl font-cabin font-bold pl-6 mb-1 tablet:mb-3">
-                <Titulo {titulo} />
-              </h2>
+        {#if titulo != ''}
+          {#if id == '1'}
+            <h2 class="text-3xl tablet:text-6xl font-cabin font-bold pl-6 mb-1 tablet:mb-3">
+              <Titulo {titulo} />
+            </h2>
           {/if}
           {#if id != '1'}
-              <h2 class="text-4xl font-cabin font-bold pl-6">
-                <Titulo {titulo} />
-              </h2>
+            <h2 class="text-4xl font-cabin font-bold pl-6">
+              <Titulo {titulo} />
+            </h2>
           {/if}
         {/if}
       {/if}
 
-      {#if position == "right"}
-        {#if titulo != ""}
+      {#if position == 'right'}
+        {#if titulo != ''}
           <h2 class="text-2xl font-cabin font-bold mt-3 pt-6 pl-3">
-          <Titulo {titulo} />
+            <Titulo {titulo} />
           </h2>
         {/if}
       {/if}
 
-      {#if texto != ""}
+      {#if texto != ''}
         <Texto {texto} />
       {/if}
-
     </div>
-
   </div>
 {/if}
 
@@ -66,14 +63,14 @@
   }
   .position_center {
     @apply py-10 w-[100%] tablet:w-[80%] desktop:w-[35%];
-  } 
+  }
   .wrapper_center {
-    @apply flex justify-start tablet:items-center tablet:justify-center ;
+    @apply flex justify-start tablet:items-center tablet:justify-center;
   }
   .wrapper_right {
     @apply flex justify-end p-4;
   }
-/* 
+  /* 
   .screen_5 {
      @apply flex justify-end items-center;
       transition: all ease-in-out 1s;
@@ -83,14 +80,12 @@
     @apply mt-20;
     transition: all ease-in-out 500ms;
   }
-  .fine_5  {
+  .fine_5 {
     @apply mt-40;
     transition: all ease-in-out 500ms;
   }
- .fine_6 {
+  .fine_6 {
     @apply mt-60;
     transition: all ease-in-out 500ms;
   }
- 
-
 </style>
