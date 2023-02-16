7c92cd9697ca56c08a3e449747d303b294033a60
Aitor Resano <aitor.resano@biko2.com>
2023-02-16 10:31:03 +0100

    Fix conflictos

--- src/components/menu/MenuAnimacion.svelte
+++ src/components/menu/MenuAnimacion.svelte
@@ -2,9 +2,6 @@
   import LinkUnderlineDecoratorInicio from '../ui/LinkUnderline-decorator-inicio.svelte'
 </script>
 
-<!-- Menu-->
-<div class="z-10 w-1/4 text-sm absolute top-6 left-6">
-  <nav>
-    <LinkUnderlineDecoratorInicio texto="Inicio" />
-  </nav>
-</div>
+
+<LinkUnderlineDecoratorInicio texto="Inicio" />
+
