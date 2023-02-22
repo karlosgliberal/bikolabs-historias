97a6723234962833e2da7f2478b2cdab13dc1451
Aitor Resano <aitor.resano@biko2.com>
2023-02-22 17:51:21 +0100

    WIP mobile

--- src/components/menu-interior/menu-interior.svelte
+++ src/components/menu-interior/menu-interior.svelte
@@ -1,12 +1,42 @@
 <script>
-  import LinkUnderlineDecorator from '../ui/LinkUnderline-decorator.svelte'
-  let src = 'images/logo.svg'
+  import BurgerMenu from 'svelte-burger-menu';
+  import Items from './components/Items.svelte';
+ 
+  let src = 'images/logo.svg';
+  const burguerProps = {
+    backgroundColor: 'white',
+    padding: '10',
+    width: '100%',
+  }
+
 </script>
 
-<nav class="flex items-center gap-4 text-xs tablet:text-sm">
-  <div><a href="/"><img {src} alt="" width="17px" /></a></div>
-  <LinkUnderlineDecorator texto="Inicio" link="/" isColor />
-  <LinkUnderlineDecorator texto="Versión interactiva" link="/" isColor />
-  <LinkUnderlineDecorator texto="Versión en texto" link="/no-interactive" isColor />
-  <LinkUnderlineDecorator texto="Sobre Bikolabs y los datos" link="/about" isColor />
-</nav>
+<div class="hidden tablet:inline">
+  <nav class="flex items-center gap-4 text-xs tablet:text-sm">
+    <div>
+      <a href="/"><img {src} alt="" width="17px" /></a>
+    </div>
+    <Items/>
+  </nav>
+</div>
+
+<div class ="inline tablet:hidden">
+  <div class="flex flex-row justify-between">
+    <div class="pt-3 ">
+    <BurgerMenu {...burguerProps}>
+      <div class="flex flex-col gap-5 text-2xl p-4 text-black">
+        <Items/>
+      </div>
+    </BurgerMenu>
+    </div>
+    <div class="mr-3 z-20 absolute right-1 top-2">
+      <a href="/"><img {src} alt="" width="20px" /></a>
+    </div>
+
+  </div>
+</div>
+
+
+<style> 
+
+</style>
