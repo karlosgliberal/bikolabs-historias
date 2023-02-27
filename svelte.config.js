fc123fa310b62eb1061fd09e38c180c79c1cc056
karlosgliberal <karlosgliberal@gmail.com>
2023-02-27 20:25:56 +0100

    EL NIÑO DE LOS PAQueTITOS

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
   },
-  extensions: [".svelte"],
+  extensions: ['.svelte'],
   preprocess: [
     preprocess({
       postcss: true,
     }),
   ],
-};
+}
 
-export default config;
+export default config
